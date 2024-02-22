357941fe51ef8366c56f8c40a2efe389bacbde92
Jescel James <jesceljames@gmail.com>
2024-02-22 14:33:18 -0500

    time block for each working hour changes color depending on the current time.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 $(function () {
 
   var today = dayjs();
-  var currentHour = today.hour();
+  
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -18,6 +18,24 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  $('.time-block').each(function() {
+    var currentHour = dayjs().hour();
+    var eachWorkingHour = parseInt($(this).attr('id').replace('hour-', ''));
+    
+    console.log(currentHour, eachWorkingHour
+)
+    $(this).removeClass('past present future');
+    
+    if (eachWorkingHour
+ < currentHour) {
+      $(this).addClass('past');
+    } else if (eachWorkingHour
+ === currentHour) {
+      $(this).addClass('present');
+    } else {
+      $(this).addClass('future');
+    }
+  });
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -27,6 +45,5 @@ $(function () {
  
   $('#currentDay').text(today.format('dddd, MMM D YYYY'));
 
-  console.log(currentHour)
 
 });
